27a19ae7ef39b6bd696898cfc069e12b61ba003a
Bryan Li <bzl3@cornell.edu>
2017-04-11 00:10:30 -0400

    screen block analysing

--- Image_Checker.py
+++ Image_Checker.py
@@ -7,17 +7,47 @@ class Image_Checker:
         train_cnt = 0
         th = 0
         
-
+    # need to train pytesseract!!!
     def print_str_from_img( self, path ):
         try:
             im = Image.open(path)
-#        im.show()
             result = pytesseract.image_to_string(im)
         except:
             print ("Failed to convert img to str")
             return None
         print(result)
         return result
-    
 
+    
+    def get_main_color( self, im ):
+        h = im.histogram()
+        red = 0
+        green = 0
+        blue = 0
+
+        # split into red, green, blue
+        r = h[0:256]
+        g = h[256:256*2]
+        b = h[256*2: 256*3]
+
+        for i,w in enumerate(r):
+            red = red + i*w
+        for i,w in enumerate(g):
+            green = green + i*w
+        for i,w in enumerate(b):
+            blue = blue + i*w
             
+        if ( red > blue and red > green ):
+            #print("Main color is RED!")
+            return 'r'
+        elif ( blue > red and blue > green ):
+            #print("Main color is BLUE!")
+            return 'b'
+        elif ( green > red and green > blue ):
+            #print("Main color is GREEN!")
+            return 'g'
+        else:
+            print("Failed to find main color")
+            return 'n'
+
+    
--- bot.py
+++ bot.py
@@ -1,15 +1,37 @@
 from PIL import ImageGrab
 from Screen_Module import Screen_Module
 from Image_Checker import Image_Checker
+from datetime import datetime
+import time
+import winsound
 
 if __name__ == "__main__":
     img_path = "bot1.jpg"
     sm = Screen_Module()
     ic = Image_Checker()
     sm.init_box_coordinate();
+    t = 5;
     
     # part of the screen
-    im = sm.grab()
-    im.save(img_path, "JPEG")
-#    im.show()
-    result = ic.print_str_from_img( img_path )
+    #im.save(img_path, "JPEG")
+    #im.show()
+    #result = ic.print_str_from_img( img_path )
+    
+    while (True):
+        im = sm.grab()
+        color = ic.get_main_color(im)
+        if ( 'n' == color ):
+            break
+        elif('r' == color ):
+            print( "%s : Hippo is HUNGRY!!!!" %(str(datetime.now())))
+            winsound.PlaySound("barking.wav", winsound.SND_FILENAME)
+            t = 5
+        elif('b' == color ):
+            print("Hippo is doing OK")
+            t = 30
+        else:
+            print("What happen?????")
+
+        time.sleep(t)
+        
+    
