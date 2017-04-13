f28b5908be96e350092f151c6a713c7640132482
Bryan Li <bzl3@cornell.edu>
2017-04-12 23:17:23 -0400

    add click

--- bot.py
+++ bot.py
@@ -5,6 +5,20 @@ from Mail_module import Mail_module
 from datetime import datetime
 import time
 import winsound
+import pyautogui
+import msvcrt
+
+def click(x,y):
+    pyautogui.moveTo(x, y)
+    pyautogui.click()
+    
+def get_point():
+    ret = True
+    while ret:
+        c = msvcrt.getwch()
+        if "c" == c:
+            ret = False
+    return pyautogui.position()
 
 if __name__ == "__main__":
     server = 'smtp.gmail.com:587'
@@ -13,12 +27,19 @@ if __name__ == "__main__":
     to_addr = input("Enter recipient email: ")
     mm = Mail_module( username, pw, server )
     mm.set_from("RAC Bot")
-    
+    x1 = 0
+    y1 = 0
+    x2 = 0
+    y2 = 0
     sm = Screen_Module()
     ic = Image_Checker()
     sm.init_box_coordinate();
     t = 5;
     sent = False
+    print("Place mouse on 1st point and press key \"c\"");
+    (x1,y1) = get_point();
+    print("Place mouse on 2st point and press key \"c\"");
+    (x2,y2) = get_point();
 
     while (True):
         im = sm.grab()
@@ -30,7 +51,9 @@ if __name__ == "__main__":
             if ( not sent ):
                 mm.sendmail(to_addr, "Hippo's hungry!", str(datetime.now()))
                 sent = True
-            #winsound.PlaySound("barking.wav", winsound.SND_FILENAME)
+            click(x1, y1)
+            time.sleep(2)
+            click(x2, y2)
             t = 5
         elif('b' == color ):
             print("%s : Hippo is doing OK" %(str(datetime.now())))
